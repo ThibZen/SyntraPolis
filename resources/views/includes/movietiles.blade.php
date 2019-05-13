8616aa17516338e469ac579876a1873563abd729
Arno <39265533+DeviousWolf4@users.noreply.github.com>
2019-05-13 13:42:47 +0200

    Template for movie tiles

---
+++ resources/views/includes/movietiles.blade.php
@@ -0,0 +1,7 @@
+<section class="tilecontainer">
+    <!-- For each item pulled from the database, create a div like this -->
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+    <div class="image"></div>
+</section>
