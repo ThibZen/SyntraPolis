0e8af5da7ce2d24773f054497c06fc3ece186cc9
Arno <39265533+DeviousWolf4@users.noreply.github.com>
2019-05-13 13:43:08 +0200

    All templates included in homepage

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -9,6 +9,12 @@
             @include('includes.navbanner')
             @include('includes.botnavigation')
         </header>
-    
+        <div class="container">
+            @include('includes.movietiles')
+            @include('includes.banner')
+        </div>
+        <footer>
+            @include('includes.footer')
+        </footer>
     </body>
 </html>
