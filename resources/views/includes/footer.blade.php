0ca7129b23d721da2ea439dd2f63dd26a7d57ff1
Arno <39265533+DeviousWolf4@users.noreply.github.com>
2019-05-13 13:42:18 +0200

    Template for footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,14 @@
+<div class="footer">
+    <p>
+        Sign up for our newsletter
+    </p>
+    <form method="POST">
+        <input type="text" placeholder="john.doe@example.com">
+        <input type="submit" value="Sign up">
+    </form>
+    <ul>
+        <li> instagram </li>
+        <li> twitter </li>
+        <li> facebook </li>
+    </ul>
+</div>
