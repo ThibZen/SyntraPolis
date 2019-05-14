1484cd00b957d665e251e472241d5d5dd633319d
Arno <39265533+DeviousWolf4@users.noreply.github.com>
2019-05-14 14:23:28 +0200

    Added head blade

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,8 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="keywords" content="SyntraPolis,Syntra,Full Stack Developer, Films">
+    <meta name="description" content="Welcome to SyntraPolis">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <title>SyntraPolis - @yield('title')</title>
+</head>
