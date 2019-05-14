7c3d57dfaeb11ada6ab14aedc2b4dc9cb763fed1
Arno <39265533+DeviousWolf4@users.noreply.github.com>
2019-05-14 15:01:53 +0200

    Created schedule template

--- resources/views/includes/movietiles.blade.php
+++ resources/views/includes/movietiles.blade.php
@@ -1,7 +1,18 @@
 <section class="tilecontainer">
     <!-- For each item pulled from the database, create a div like this -->
-    <div class="image"></div>
-    <div class="image"></div>
-    <div class="image"></div>
-    <div class="image"></div>
+    <div class="movie">
+        <div class="image"></div>
+    </div>
+    <div class="movie">
+        <div class="image"></div>
+    </div>
+    <div class="movie">
+        <div class="image"></div>
+    </div>
+    <div class="movie">
+        <div class="image"></div>
+    </div>
+    <div class="movie">
+        <div class="image"></div>
+    </div>
 </section>
---
+++ resources/views/includes/movietilesschedule.blade.php
@@ -0,0 +1,48 @@
+<section class="tilecontainer">
+    <!-- For each item pulled from the database, create a div like this -->
+    <div class="movie">
+        <div class="image"></div>
+        <p> Time </p>
+        <ul>
+            <li> 17h00 </li>
+            <li> 19h00 </li>
+            <li> 21h00 </li>
+        </ul>
+    </div>
+    <div class="movie">
+        <div class="image"></div>
+        <p> Time </p>
+        <ul>
+            <li> 17h00 </li>
+            <li> 19h00 </li>
+            <li> 21h00 </li>
+        </ul>
+    </div>
+    <div class="movie">
+        <div class="image"></div>
+        <p> Time </p>
+        <ul>
+            <li> 17h00 </li>
+            <li> 19h00 </li>
+            <li> 21h00 </li>
+        </ul>
+    </div>
+    <div class="movie">
+        <div class="image"></div>
+        <p> Time </p>
+        <ul>
+            <li> 17h00 </li>
+            <li> 19h00 </li>
+            <li> 21h00 </li>
+        </ul>
+    </div>
+    <div class="movie">
+        <div class="image"></div>
+        <p> Time </p>
+        <ul>
+            <li> 17h00 </li>
+            <li> 19h00 </li>
+            <li> 21h00 </li>
+        </ul>
+    </div>
+</section>
