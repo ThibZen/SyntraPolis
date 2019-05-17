9c04d24035ffeb14f71392f416e5f27f0160dd34
Matthias vdb <you@example.com>
2019-05-17 16:50:57 +0200

    Db commit poging 4

--- resources/db/MovieDatabase.sql
+++
@@ -1,9 +0,0 @@
-SELECT M.Title, G.genre, CONCAT(A.FIRSTName, ' ', A.LastName) AS ActorFullName, CONCAT (D.FirstName, ' ', D.LastName) AS DirectorFullName,  PR.PegiName FROM movie M
-JOIN movieactors MA ON MA.MovieID = M.MovieID
-JOIN moviedirectors MD ON MD.MovieID = M.MovieID
-JOIN actor A ON A.ActorID = MA.ActorID
-JOIN director D ON D.DirectorID = MD.DirectorID
-JOIN moviepegi MP ON MP.MovieID = M.MovieID
-JOIN pegirating PR ON PR.PegiID = MP.PegiID
-JOIN moviegenre MR on MR.MovieID = M.MovieID
-JOIN genre G ON G.GenreID = MR.GenreID
