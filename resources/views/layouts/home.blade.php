16135a914f1bbc883986ddb4c9f0c5db31551608
Arno De Leeuw <39265533+DeviousWolf4@users.noreply.github.com>
2019-06-03 16:00:54 +0200

    Load in movieposter from db on homepage

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -6,15 +6,25 @@
 @endsection
 
 @section('content')
-    @forelse($movies as $movie)
+    {{--@forelse($movies as $movie)
         <a href="#">
-            <img src="{{ asset('db/Poster/'.$movie->Foto.'.jpg') }}">
+            <img src="{{ asset('../resources/db/Poster/'.$movie->Foto.'.jpg') }}">
         </a>
     @empty
         <div>
             <p>There are no movies</p>
         </div>
-    @endforelse
+    @endforelse--}}
+    
+    <section class="tilecontainer">
+    @for ($i = 0; $i < 8; $i++)
+        <div class="movie">
+            <a href="#">
+                <img src="{{ asset('../resources/db/Poster/'.$movies[$i]->Foto.'.jpg') }}">
+            </a>
+        </div>
+    @endfor
+    </section>
 
     @include('includes.banner')
 @endsection
