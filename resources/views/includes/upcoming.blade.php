f66f95387b7397e365c42acfbc19f20cdfbd93ad
Arno De Leeuw <39265533+DeviousWolf4@users.noreply.github.com>
2019-06-04 10:13:24 +0200

    Added styling for upcoming page

--- resources/views/includes/upcoming.blade.php
+++ resources/views/includes/upcoming.blade.php
@@ -1,5 +1,5 @@
 <div class="upcomingmovie">
-    <img>
+    <img src="images/movieposter_1.jpg">
     <h2> Movie Title </h2>
     <h3> Release Date </h3>
 
@@ -7,8 +7,39 @@
         This is where the description will appear. Use different "p" tags to make alineas
     </p>
     <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec tempus ex. Nunc consectetur a enim quis interdum. Quisque nulla magna, pretium at tellus vitae, hendrerit malesuada tortor. Mauris erat nunc, volutpat nec dictum ut, pharetra quis est. Duis finibus est nec augue mollis, in volutpat ligula dignissim. Suspendisse mauris nibh, ultrices commodo dolor efficitur, tristique fermentum felis. Nullam efficitur dolor pharetra neque ullamcorper tristique. Duis ornare massa eu ultrices vulputate. Pellentesque et placerat quam, id eleifend nisi. Sed vel odio felis. Suspendisse a sodales lorem. In nec fermentum lorem. Curabitur commodo augue varius, mattis nisl luctus, pellentesque mauris. Aliquam finibus purus massa, at ornare nisi aliquam quis. Pellentesque eget turpis eu justo bibendum volutpat.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec tempus ex. Nunc consectetur a enim quis interdum. Quisque nulla magna, pretium at tellus vitae, hendrerit malesuada tortor. Mauris erat nunc, volutpat nec dictum ut, pharetra quis est. Duis finibus est nec augue mollis
     </p>
     
     <div class="readmore"></div>
 </div>
+
+<div class="upcomingmovie">
+        <img src="images/movieposter_1.jpg">
+        <h2> Movie Title </h2>
+        <h3> Release Date </h3>
+    
+        <p>
+            This is where the description will appear. Use different "p" tags to make alineas
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec tempus ex. Nunc consectetur a enim quis interdum. Quisque nulla magna, pretium at tellus vitae, hendrerit malesuada tortor. Mauris erat nunc, volutpat nec dictum ut, pharetra quis est. Duis finibus est nec augue mollis
+        </p>
+        
+        <div class="readmore"></div>
+    </div>
+
+    <div class="upcomingmovie">
+            <img src="images/movieposter_1.jpg">
+            <h2> Movie Title </h2>
+            <h3> Release Date </h3>
+        
+            <p>
+                This is where the description will appear. Use different "p" tags to make alineas
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec tempus ex. Nunc consectetur a enim quis interdum. Quisque nulla magna, pretium at tellus vitae, hendrerit malesuada tortor. Mauris erat nunc, volutpat nec dictum ut, pharetra quis est. Duis finibus est nec augue mollis
+            </p>
+            
+            <div class="readmore"></div>
+        </div>
+        
