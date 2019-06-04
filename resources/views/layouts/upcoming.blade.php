c24592db559b42af38772216db5f7ae513319b50
Arno De Leeuw <39265533+DeviousWolf4@users.noreply.github.com>
2019-06-04 11:21:58 +0200

    Fill upcoming page with content from DB

--- resources/views/layouts/upcoming.blade.php
+++ resources/views/layouts/upcoming.blade.php
@@ -3,6 +3,17 @@
 @section('content')        
     <h1> Upcoming Movies </h1>
     <section class="upcomingcontainer">
-        @include('includes.upcoming')
+            @for ($i = 0; $i < 3; $i++)
+            <div class="upcomingmovie">
+                    <img src="{{ asset('/images/Poster/'.$movies[$i]->Foto.'.jpg') }}">
+                    <h2> {{$movies[$i]->Title}} </h2>
+                    <h3> {{ date('d-m-Y', strtotime($movies[$i]->ReleaseDate))}} </h3>
+                
+                    <p>
+                        {{$movies[$i]->DescriptionLong}}
+                    </p>                    
+                    <div class="readmore"></div>
+                </div>
+        @endfor
     </section>
 @endsection
