e08d836be2c827016c2c0e99aaf618e7c1698cdc
Matthias vdb <you@example.com>
2019-06-04 12:55:00 +0200

    insert form blade adjustments

--- app/Http/Controllers/DBController.php
+++ app/Http/Controllers/DBController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class DBController extends Controller
 {
@@ -41,8 +42,8 @@ class DBController extends Controller
 
     function record_exists_movie ($table, $column, $value)
     {
-        $result = DB::select("SELECT * FROM {$table} WHERE {$column} LIKE '$value'", [1]);
-        if($result->num_rows == 0) {
+        $result = DB::select("SELECT * FROM {$table} WHERE {$column} = '$value'");
+        if(sizeof($result) == 0) {
             $output = ' nothing found';
             echo $output;
             return false;
@@ -56,7 +57,7 @@ class DBController extends Controller
     function record_exists_names ($table, $value)
     {
         $result = DB::select("SELECT * FROM {$table} WHERE CONCAT(FirstName,' ',LastName) LIKE '%$value%'", [1]);
-        if($result->num_rows == 0) {
+        if(sizeof($result) == 0) {
             $output = ' nothing found';
             echo $output;
             return false;
@@ -76,13 +77,13 @@ class DBController extends Controller
             $sqlActor = "INSERT INTO actor (FirstName, LastName) VALUES ('$actorfirst','$actorlast')";
             if(DB::insert('$sqlActor')){
                 echo " Actor records added successfully.";
-                $actor_id = sqlite_last_insert_rowid();
+                $actor_id = DB::getPdo()->lastInsertId();;
             } else {
                 echo "ERROR: Could not able to execute $sqlActor. " . sqlite_last_error();
             }
         } else {
             //RECORD EXISTS
-            $result = (DB::insert("SELECT ActorID FROM actor WHERE CONCAT(FirstName,' ',LastName) LIKE '%$actorfull%'"))->fetch_all()[0];
+            $result = (DB::select("SELECT ActorID FROM actor WHERE CONCAT(FirstName,' ',LastName) LIKE '%$actorfull%'"))->fetch_all()[0];
             $actor_id = $result[0];
         }
         return $actor_id;
@@ -97,15 +98,15 @@ class DBController extends Controller
         if ($this->record_exists_names('director', $directorfull) == false) {
             //RECORD DOESNT EXIST
             $sqlDirector = "INSERT INTO director (FirstName, LastName) VALUES ('$directorfirst','$directorlast')";
-            if (DB::insert('$sqlDirector')){
+            if (DB::insert($sqlDirector)){
                 echo "Director records added successfully.";
-                $director_id = sqlite_last_insert_rowid();
+                $director_id = DB::getPdo()->lastInsertId();;
             } else {
                 echo "ERROR: Could not able to execute $sqlDirector. " . sqlite_last_error();
             }
         } else {
             //RECORD EXISTS
-            $result = (DB::insert("SELECT DirectorID FROM director WHERE CONCAT(FirstName,' ',LastName) LIKE '%$directorfull%'"))->fetch_all()[0];
+            $result = (DB::select("DirectorID FROM director WHERE" DB::raw('CONCAT(FirstName,' ',LastName)')" LIKE '%$directorfull%'"))->fetch_all()[0];
             $director_id = $result[0];
         }
         return $director_id;
@@ -114,7 +115,7 @@ class DBController extends Controller
     //Inserts
     function movieActorsInsert($movie_id,$actor_id){
         $sql = "INSERT INTO movieactors (MovieID, ActorID) VALUES (" . $movie_id . "," . $actor_id . ")";
-        if (DB::insert('$sql')){
+        if (DB::insert($sql)){
             echo " tussentabel actors gelukt";
         } else {
             echo "ERROR: Could not able to execute $sql. " . sqlite_last_error();
@@ -124,7 +125,7 @@ class DBController extends Controller
     function movieDirectorsInsert($movie_id,$director_id)
     {
         $sql = "INSERT INTO moviedirectors (MovieID, DirectorID) VALUES (" . $movie_id . "," . $director_id . ")";
-        if (DB::insert('$sql')){
+        if (DB::insert($sql)){
             echo " tussentabel directors gelukt";
         } else {
             echo "ERROR: Could not able to execute $sql. " . sqlite_last_error();
@@ -135,7 +136,7 @@ class DBController extends Controller
     {
         $genre = $req->input ('genre');
         $sql = "INSERT INTO moviegenre (MovieID, GenreID) VALUES (" . $movie_id . "," . $genre . ")";
-        if (DB::insert('$sql')){
+        if (DB::insert($sql)){
             echo " tussentabel genre gelukt";
         } else {
             echo "ERROR: Could not able to execute $sql. " . sqlite_last_error();
@@ -146,7 +147,7 @@ class DBController extends Controller
     {
         $pegi = $req->input ('pegi');
         $sql = "INSERT INTO moviepegi (MovieID, PegiID) VALUES (" . $movie_id . "," . $pegi . ")";
-        if (DB::insert('$sql')){
+        if (DB::insert($sql)){
             echo " tussentabel pegi gelukt";
         } else {
             echo "ERROR: Could not able to execute $sql. " . sqlite_last_error();
@@ -168,11 +169,11 @@ class DBController extends Controller
         }else{
             $sqlMovie = "INSERT INTO movie (Title, DescriptionShort, DescriptionLong, Runtime, ReleaseDate, Foto, Status, IMDBLink )
             VALUES ('$movie_title','$short_descr','$long_descr','$runtime','$releasedate','$poster','$status','$imdb')";
-            if(DB::insert('$sqlMovie')){
+            if(DB::insert($sqlMovie)){
                 echo "Movie records added successfully.";
-                $movie_id = sqlite_last_insert_rowid();
-                $actor_id = $this->actorInsert();
-                $director_id = $this->directorInsert();
+                $movie_id = DB::getPdo()->lastInsertId();
+                $actor_id = $this->actorInsert($req);
+                $director_id = $this->directorInsert($req);
                 $this->movieActorsInsert($movie_id,$actor_id);
                 $this->movieDirectorsInsert($movie_id,$director_id);
                 $this->movieGenreInsert($movie_id);
