eac9a34d9d300534f53514215f2d8888e81d059b
Arno De Leeuw <39265533+DeviousWolf4@users.noreply.github.com>
2019-06-06 13:18:21 +0200

    Added text to contact page + small design edits

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -3,8 +3,18 @@
 @section('content')
     <h1> Want to contact us? </h1>
     <div class="contact">
-        <div class="contactform">
-            @include('includes.contactform')
+        <div class="contactleft">
+            <article>
+                <p>
+                    We value customer satisfaction very highly here at SyntraPolis. Day and night we strive to give you, our beloved customer, the best possible viewing experience. If you have any suggestions on what we could improve or you have any questions feel free to use the form located below to relay them to us and we will do our best to respond to you as soon as possible.
+                </p>
+                <p>
+                    Have a more pressing matter you need to discuss? You'll find our phone number along with our email adress on this page aswell. We hope to hear from you soon!
+                </p>
+            </article>
+            <div class="contactform">
+                @include('includes.contactform')
+            </div>
         </div>
         <div class="contactright">
             @include('includes.maps')
