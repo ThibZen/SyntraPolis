e08e532a4b08558fbad11da3b3252d1c3b073e1d
Arno De Leeuw <39265533+DeviousWolf4@users.noreply.github.com>
2019-06-20 13:49:59 +0200

    Prevented user from changing title

--- resources/views/layouts/ordermovie.blade.php
+++ resources/views/layouts/ordermovie.blade.php
@@ -6,7 +6,7 @@
 <form class="orderform" autocomplete="off" action="{{ route('orderStore') }}" method="POST">
         @csrf
         <label for="title">Title</label>
-        <input name="Title" id="title" type="text" placeholder="Title" autocomplete="off" value="{{ $movie->Title }}" onkeypress="return false;"><br>
+        <input name="Title" id="title" type="text" placeholder="Title" autocomplete="off" value="{{ $movie->Title }}" disabled="disabled"><br>
 
         <label for="date">Date</label>
         <input name="Date" id="date" type="text" placeholder="Choose Date" autocomplete="off" value="{{ old('Date') }}"><br>
